a2fefd4e505a369f207e124655dd1e71a338d898
panos <p.oikonomidis.92@gmail.com>
2023-01-25 18:38:21 +0200

    Initial

--- AksiApi02/Program.cs
+++ AksiApi02/Program.cs
@@ -1,13 +1,7 @@
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
-namespace AksiApi02
+namespace AksiApi
 {
     public class Program
     {
--- AksiApi02/Startup.cs
+++ AksiApi02/Startup.cs
@@ -1,17 +1,19 @@
+using AksiApi.Context;
+using AksiApi.Entities;
+using AksiApi.Repositories;
+using AksiApi.Repositories.Interfaces;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.IO;
+using System.Reflection;
 
-namespace AksiApi02
+namespace AksiApi
 {
     public class Startup
     {
@@ -25,7 +27,20 @@ namespace AksiApi02
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<AtaContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("MyDatabase")));
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Transaction API", Version = "v1" });
+                var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+                c.IncludeXmlComments(xmlPath);
+            });
+
             services.AddControllers();
+
+            services.AddTransient<IRepository<Transaction>, TransactionRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -45,6 +60,13 @@ namespace AksiApi02
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
+            });
+
+            app.UseSwagger();
+
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Transaction API V1");
             });
         }
     }
